16c9f2b7a16b0913aca038a81608dbc514f28e4c
MassiBaldi <massibaldi85@gmail.com>
2018-12-17 15:56:33 +0100

    non funziona chiamata api

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  $('#myButton').click(function(){
+    var searchValue = $('#search').val();
+    console.log(searchValue);
+
+    $.ajax({
+      url:'https://api.themoviedb.org/3/search/movie',
+      method:'GET',
+      data: {
+        api_key:'591f55f90c0e21f16b04fc51951b4a5c',
+        language:'it',
+        queary: searchValue
+      },
+      success: function(data) {
+        console.log(data);
+      },
+      error: function() {
+        alert('Errore');
+      }
+    });
+  });
+
+
+});
