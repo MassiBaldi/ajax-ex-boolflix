0480c5ff4508df5c0404f18bb4f0a3869e294389
MassiBaldi <massibaldi85@gmail.com>
2018-12-18 14:21:53 +0100

    associare stelle

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 
   $('#myButton').click(function(){
+    // alert('ciao');
     var searchValue = $('#search').val();
-    console.log(searchValue);
+    // console.log(searchValue);
 
     $.ajax({
       url:'https://api.themoviedb.org/3/search/movie',
@@ -13,7 +14,35 @@ $(document).ready(function(){
         query: searchValue
       },
       success: function(data) {
-        console.log(data);
+        // console.log(data);
+        var movies = data. results;
+
+        for (var i = 0; i < movies.length; i++) {
+          var movie = movies[i];
+
+          var source   = $('#movie-template').html();
+          var template = Handlebars.compile(source);
+
+          //divido il numero del voto e lo porto a numero intero, arrotondato per eccesso
+          var numero = movie.vote_average /2;
+          console.log(numero);
+
+          var numeroArrotondato = Math.ceil(numero);
+          console.log(numeroArrotondato);
+
+          var context = {
+            title: movie.title,
+            original_title: movie.original_title,
+            original_language: movie.original_language,
+            vote_average: numeroArrotondato
+          };
+
+          var html = template(context);
+          $('.container').append(html);
+
+
+        }
+
       },
       error: function() {
         alert('Errore');
